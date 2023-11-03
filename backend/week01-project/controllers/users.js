0258973d8e269a22abf04430a7862306af4cf97c
Felipe <felipesantos.dsb@hotmail.com>
2023-11-03 10:06:07 -0300

    week01 - Project: Contacts Part 1

---
+++ backend/week01-project/controllers/users.js
@@ -0,0 +1,19 @@
+const mongodb = require('../data/database');
+const ObjectId = require('mongodb').ObjectId;
+
+const getAll = async (req, res) => {
+    const result = await mongodb.getDb().collection('users').find(); 
+    result.toArray().then((users) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(users);
+    })
+};
+
+const getSingle = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().collection('users').findOne({ _id: userId }); 
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(result); 
+}
+
+module.exports = { getAll, getSingle };
