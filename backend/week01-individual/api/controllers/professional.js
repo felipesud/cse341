cee272615876280c140a9927101e5a7b4891ae67
Felipe <felipesantos.dsb@hotmail.com>
2023-11-03 10:23:40 -0300

    week01 - Project: Contacts Part 1

--- backend/week01-individual/api/controllers/professional.js
+++
@@ -1,9 +0,0 @@
-const mongodb = require('../db/connect');
-
-const getData = async (req, res, next) => {
-    const result = await mongodb.getDb().collection('user').find().toArray();
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(result);
-};
-
-module.exports = { getData };
