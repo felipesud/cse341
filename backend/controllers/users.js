7e9881ff7a6881aafc1f73a7b80dfcc2ce8d5808
Felipe <felipesantos.dsb@hotmail.com>
2023-11-11 07:05:51 -0300

    week02 - Project: Contacts Part 2

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -3,11 +3,12 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
     //#swagger.tags = ['Users']
-    const result = await mongodb.getDb().collection('users').find(); 
-    result.toArray().then((users) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users);
-    })
+    try {
+        const result = await mongodb.getDb().collection('users').find().toArray();
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ error: 'Error retrieving users' });
+    }
 };
 
 const getSingle = async (req, res) => {
@@ -18,11 +19,13 @@ const getSingle = async (req, res) => {
         return;
     }
     const userId = new ObjectId(id);
-    const result = await mongodb.getDb().collection('users').findOne({ _id: userId }); 
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(result); 
+    try {
+        const result = await mongodb.getDb().collection('users').findOne({ _id: userId });
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ error: 'Error retrieving user' });
+    }
 };
-
 
 const createUser = async (req, res) => {
     //#swagger.tags = ['Users']
@@ -33,11 +36,12 @@ const createUser = async (req, res) => {
         favoriteColor: req.body.favoriteColor,
         birthday: req.body.birthday
     };
-    const response = await mongodb.getDb().collection('users').insertOne(user);
-    if(response.acknowledged > 0){
-        res.status(204).send();
-    }else {
-        res.status(500).json(response.error || 'Some error ocurred while creating the user');
+
+    try {
+        const response = await mongodb.getDb().collection('users').insertOne(user);
+        res.status(200).json({ message: 'User created successfully', userId: response.insertedId });
+    } catch (error) {
+        res.status(500).json({ error: 'Error creating user' });
     }
 };
 
@@ -51,20 +55,33 @@ const updateUser = async (req, res) => {
         favoriteColor: req.body.favoriteColor,
         birthday: req.body.birthday    
     };
-    const response = await mongodb.getDb().collection('users').replaceOne({ _id: userId }, user );
+
+    try {
+        const response = await mongodb.getDb().collection('users').replaceOne({ _id: userId }, user);
+        if (response.modifiedCount > 0) {
+            res.status(200).json({ message: 'User updated successfully', userId: response.insertedId });
+        } else {
+            res.status(500).json({ error: 'No user found to update' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Error updating user' });
+    }
 };
 
 const deleteUser = async (req, res) => {
     //#swagger.tags = ['Users']
     const userId = new ObjectId(req.params.id);
-    const response = await mongodb.getDb().collection('users').deleteOne({ _id: userId });
-    if(response.acknowledged > 0){
-        res.status(204).send();
-    }else {
-        res.status(500).json(response.error || 'Some error ocurred while deleting the user');
+
+    try {
+        const response = await mongodb.getDb().collection('users').deleteOne({ _id: userId });
+        if (response.deletedCount > 0) {
+            res.status(200).json({ message: 'User deleted successfully' });
+        } else {
+            res.status(500).json({ error: 'No user found to delete' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Error deleting user' });
     }
-}
-
-
+};
 
 module.exports = { getAll, getSingle, createUser, updateUser, deleteUser };
