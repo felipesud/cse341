01c9125969bdcdee8a4427e12600d55ef4f2d87d
Felipe <felipesantos.dsb@hotmail.com>
2023-11-11 21:41:45 -0300

    week02 - Project: Contacts Part 2

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,36 @@
 const express = require("express");
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
-const mongodb = require('./data/database');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+const mongodb = require("./data/database");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Z-key"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
 
+app.use("/", require("./routes"));
 
-  app.use('/', require('./routes'));
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-key');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  next();
-});
-
-
-
 mongodb.initDb((err) => {
-  if(err){
+  if (err) {
     console.log(err);
-  }else {
+  } else {
     app.listen(port, () => {
       console.log(`Database is  listening and running on port ${port}`);
     });
   }
-})
-
-
+});
