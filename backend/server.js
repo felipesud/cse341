c3d216e4363f345b576db3e70fcb290df6615814
Felipe <felipesantos.dsb@hotmail.com>
2023-11-11 21:19:52 -0300

    week02 - Project: Contacts Part 2

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,12 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type,Accept, Z-key');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 
 
 
