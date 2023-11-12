4c3bdf471cda034173b25e2f99503d1e1268c742
Felipe <felipesantos.dsb@hotmail.com>
2023-11-12 00:19:44 -0300

    week02 - Project: Contacts Part 2

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,38 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
+const express = require("express");
+const cors = require("cors");
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
-const bodyParser = require("body-parser");
-const mongodb = require("./data/database");
+const mongodb = require('./data/database');
+const app = express();
+const bodyParser = require('body-parser');
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type,Accept, Z-key');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    next();
-  })
-  
+app.use('/', require('./routes'));
+
 app
   .use(cors())
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
   .use('/', require('./routes'));
 
-const port = process.env.PORT || 3000;
 
 mongodb.initDb((err) => {
-  if (err) {
+  if(err){
     console.log(err);
-  } else {
+  }else {
     app.listen(port, () => {
       console.log(`Database is  listening and running on port ${port}`);
     });
   }
-});
+})
+
