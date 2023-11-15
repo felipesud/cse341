ebac521b146cc3ef20090c416385afe9fe3003cd
Felipe <felipesantos.dsb@hotmail.com>
2023-11-15 16:16:08 -0300

    week 03 - Team Activity: Validation and Error Handling

--- backend/server.js
+++ backend/server.js
@@ -8,16 +8,20 @@ const bodyParser = require('body-parser');
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-const port = process.env.PORT || 3000;
-app.use(bodyParser.json());
-app.use((req, res, next) => {
+const port = process.env.PORT || 8083;
+app
+.use(bodyParser.json())
+.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type,Accept, Z-key');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
-});
+})
+.use('/', require('./routes'));
 
-app.use('/', require('./routes'));
+process.on('uncaughtException', (err, origin) => {
+  console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
+});
 
 app
   .use(cors())
