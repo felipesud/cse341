921226d6112e45282173b14a936e09ec8541fac9
Felipe <felipesantos.dsb@hotmail.com>
2023-11-18 09:54:23 -0300

    week 03 -Project: Quiz Part 1

---
+++ backend/middleware/validate.js
@@ -0,0 +1,24 @@
+const validator = require('../helpers/validate');
+
+const saveQuiz = (req, res, next) => {
+    const validationRule = {
+        question: 'required|string',
+        choices: 'required|array',
+        answer: 'required|string'
+    };
+    validator(req.body, validationRule, {}, (err, status) => {
+        if(!status){
+            res.status(412).send({
+                success: false,
+                message: 'Validation failed',
+                data: err
+            });
+        }else{
+            next();
+        }
+    });
+};
+
+module.exports = {
+    saveQuiz
+};
